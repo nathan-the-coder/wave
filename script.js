0fc05669780429bb6de1eddf7b597eaef0e5d5d3
Harvey <harveyjwilliams@aol.co.uk>
2020-05-12 15:21:46 +0200

    Added CustomStartPage stuff and made it dynamic. Fixed CSS bug.

--- script.js
+++ script.js
@@ -15,3 +15,44 @@ function updateClock() {
 }
 
 updateClock();
+
+function setup(data) {
+    function createLinks(linkGroups) {
+        const outerBox = document.querySelector('.box');
+
+        function createLinkContainer(linkGroup) {
+            const linkContainer = document.createElement('div');
+            linkContainer.classList.add('box-container');
+
+            const heading = document.createElement('div');
+            heading.classList.add('heading');
+            heading.innerHTML = linkGroup.name;
+
+            linkContainer.appendChild(heading);
+
+            for (let link of linkGroup.links) {
+                const anchor = document.createElement('a');
+                anchor.setAttribute('href', link.url);
+
+                anchor.innerHTML = link.name;
+
+                linkContainer.appendChild(anchor);
+            }
+
+            return linkContainer;
+        }
+
+        for (let linkGroup of linkGroups) {
+            const linkContainer = createLinkContainer(linkGroup);
+
+            outerBox.appendChild(linkContainer);
+        }
+    }
+
+    createLinks(data.linkGroups);
+}
+
+new CustomStartStorage().get()
+    .then(data => {
+        setup(data);
+    });
